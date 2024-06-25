321ccdfb432dd3f9623c380bd9afab9672d9f7ca
EsteAdonis 2025 <julio.e.ochoa.p@gmail.com>
2024-06-24 22:16:28 -0600

    "slotchange" * Getting Access to Slot Content

--- Complex-Component/modal.js
+++ Complex-Component/modal.js
@@ -76,6 +76,11 @@ class Modal extends HTMLElement {
         </section>
       </div>
     `;
+
+    const slots = this.shadowRoot.querySelectorAll('slot');
+    slots[1].addEventListener('slotchange', event => {
+      console.dir(slots[1].assignedNodes());
+    })
   }
 
 }
